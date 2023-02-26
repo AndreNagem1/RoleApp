744066ae848af06ec424103a17e5eac8062f412f
Andre Nagem <andre.nagem@shopper.com.br>
2023-02-26 11:51:54 -0300

    now it is finished, perfect

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,106 +16,104 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: ColorsRoleSp.background,
-        child: Column(
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: 350,
-              child: Stack(
-                children: [
-                  Image.asset(
-                    'assets/images/city.jpg',
-                    fit: BoxFit.fill,
-                    height: 350,
+      backgroundColor: ColorsRoleSp.background,
+      body: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 350,
+            child: Stack(
+              children: [
+                Image.asset(
+                  'assets/images/city.jpg',
+                  fit: BoxFit.fill,
+                  height: 350,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    'assets/images/big_logo.png',
+                    height: 100,
                   ),
-                  Container(
-                    alignment: Alignment.center,
-                    child: Image.asset(
-                      'assets/images/big_logo.png',
-                      height: 100,
+                ),
+                const HomeGradientEffect(),
+              ],
+            ),
+          ),
+          const SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15),
+            child: Column(
+              children: [
+                HomeSearchBar(
+                  navigateToHome: () {
+                    _navigateToMapScreen(context);
+                  },
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    HomeIcon(
+                      icon: Icons.restaurant,
+                      title: 'Restaurante',
                     ),
-                  ),
-                  const HomeGradientEffect(),
-                ],
-              ),
+                    SizedBox(width: 5),
+                    HomeIcon(
+                      icon: Icons.fastfood_rounded,
+                      title: 'FastFood',
+                    ),
+                    SizedBox(width: 5),
+                    HomeIcon(
+                      icon: Icons.coffee_rounded,
+                      title: 'Cafés',
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    HomeIcon(
+                      icon: Icons.directions_run,
+                      title: 'Ar livre',
+                    ),
+                    SizedBox(width: 5),
+                    HomeIcon(
+                      icon: Icons.event_available,
+                      title: 'Eventos',
+                    ),
+                    SizedBox(width: 5),
+                    HomeIcon(
+                      icon: Icons.shopping_bag_outlined,
+                      title: 'Shopping',
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    HomeIcon(
+                      icon: Icons.local_drink_outlined,
+                      title: 'Bar',
+                    ),
+                    SizedBox(width: 5),
+                    HomeIcon(
+                      icon: Icons.map_outlined,
+                      title: 'Mapa',
+                    ),
+                    SizedBox(width: 5),
+                    HomeIcon(
+                      icon: Icons.star,
+                      title: 'Favoritos',
+                    ),
+                  ],
+                )
+              ],
             ),
-            const SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.only(left: 15, right: 15),
-              child: Column(
-                children: [
-                  HomeSearchBar(
-                    navigateToHome: () {
-                      _navigateToMapScreen(context);
-                    },
-                  ),
-                  const SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      HomeIcon(
-                        icon: Icons.restaurant,
-                        title: 'Restaurante',
-                      ),
-                      SizedBox(width: 5),
-                      HomeIcon(
-                        icon: Icons.fastfood_rounded,
-                        title: 'FastFood',
-                      ),
-                      SizedBox(width: 5),
-                      HomeIcon(
-                        icon: Icons.coffee_rounded,
-                        title: 'Cafés',
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      HomeIcon(
-                        icon: Icons.directions_run,
-                        title: 'Ar livre',
-                      ),
-                      SizedBox(width: 5),
-                      HomeIcon(
-                        icon: Icons.event_available,
-                        title: 'Eventos',
-                      ),
-                      SizedBox(width: 5),
-                      HomeIcon(
-                        icon: Icons.shopping_bag_outlined,
-                        title: 'Shopping',
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      HomeIcon(
-                        icon: Icons.local_drink_outlined,
-                        title: 'Bar',
-                      ),
-                      SizedBox(width: 5),
-                      HomeIcon(
-                        icon: Icons.map_outlined,
-                        title: 'Mapa',
-                      ),
-                      SizedBox(width: 5),
-                      HomeIcon(
-                        icon: Icons.star,
-                        title: 'Favoritos',
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
