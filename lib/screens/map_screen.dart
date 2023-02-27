6578eb66ac1245d419abb544cd4efbc1a0caca8e
Andre Nagem <andre.nagem@shopper.com.br>
2023-02-27 15:28:07 -0300

    added nearby oplaces model

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:rolesp/Controllers/map_controller.dart';
+import 'package:http/http.dart' as http;
 
 class MapScreen extends StatelessWidget {
-  const MapScreen({ Key? key}) : super(key: key);
+  const MapScreen({Key? key}) : super(key: key);
+  final apiKey = 'AIzaSyAeFQsZFQ1uTHm53Brfxu4AH3R8JBHvj9M';
 
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(MapController());
-
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -55,6 +57,7 @@ class MapScreen extends StatelessWidget {
           myLocationEnabled: true,
           markers: controller.markers,
           onTap: (geoLocation) {
+            getNearByPlaces();
             var marker = Marker(
                 markerId: const MarkerId('1'),
                 position: LatLng(geoLocation.latitude, geoLocation.longitude));
@@ -63,5 +66,21 @@ class MapScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void getNearByPlaces() async {
+    var position = await Geolocator.getCurrentPosition();
+    var url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' +
+            position.latitude.toString() +
+            ',' +
+            position.longitude.toString() +
+            '&radius=' +
+            '30' +
+            '&key=' +
+            apiKey);
+
+    var response = await http.post(url);
+    debugPrint('MEU PRINT' + response.toString());
   }
 }
