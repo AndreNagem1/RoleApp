4c162b8513be0530b11abc9933ef0fe7ebfa84f1
AndreNagem1 <andre.nagem1@gmail.com>
2023-10-12 17:59:58 -0300

    added theming as a feature

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:rolesp/main/main_cubit.dart';
 import 'package:rolesp/screens/splash_screen/ui/SplachScreen.dart';
+
+import '../theme/roleTheme.dart';
+import '../theme/themeManager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const RoleSpApp());
 }
+
+ThemeManager themeManager = ThemeManager();
+ThemeManager mainCubit = ThemeManager(false);
 
 class RoleSpApp extends StatelessWidget {
   const RoleSpApp({super.key});
@@ -18,13 +25,13 @@ class RoleSpApp extends StatelessWidget {
       title: 'RoleSp',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        colorScheme: lightColorScheme,
       ),
       darkTheme: ThemeData(
-        brightness: Brightness.dark,
+        colorScheme: darkColorScheme,
       ),
-      themeMode: ThemeMode.light,
-      home: const SplashScreen(),
+      themeMode: themeManager.themeMode,
+      home: SplashScreen(mainCubit: mainCubit),
     );
   }
 }
