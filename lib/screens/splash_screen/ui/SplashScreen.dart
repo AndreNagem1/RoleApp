ff19fbe104d88366623dd9f6029795bcd8230efd
AndreNagem1 <andre.nagem1@gmail.com>
2024-08-19 14:19:28 -0300

    changed app icon

--- lib/screens/splash_screen/ui/SplashScreen.dart
+++ lib/screens/splash_screen/ui/SplashScreen.dart
@@ -7,6 +7,8 @@ import 'package:rolesp/main_cubit.dart';
 import 'package:rolesp/screens/splash_screen/domain/cubit/SplashCubit.dart';
 import 'package:rolesp/screens/splash_screen/domain/states/SplashScreenState.dart';
 
+import '../../events_screen/ui/events_background.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -20,34 +22,38 @@ class SplashScreen extends StatelessWidget {
       body: SizedBox(
         width: double.infinity,
         height: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/big_logo.png',
-              height: 80,
-            ),
-            const SizedBox(height: 10),
-            CircularProgressIndicator(
-              color: Theme.of(context).colorScheme.onSurface,
-            ),
-            BlocBuilder<SplashCubit, SplashScreenState>(
-                bloc: cubit,
-                builder: (context, state) {
-                  if (state is SplashScreenInitialtate) {
-                    cubit.loadNearbyPlaces();
-                  }
-                  if (state is SplashCompleteSearch) {
-                    if (mainCubit.navigateToHome) {
-                      _navigateToMain(mainCubit.selectedBottomNavItem);
-                    } else {
-                      _scheduleNavigateLogin();
+        child: CustomPaint(
+          size: const Size(double.infinity, double.infinity),
+          painter: VectorBackgroundPainter(context),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/big_logo.png',
+                height: 80,
+              ),
+              const SizedBox(height: 10),
+              CircularProgressIndicator(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+              BlocBuilder<SplashCubit, SplashScreenState>(
+                  bloc: cubit,
+                  builder: (context, state) {
+                    if (state is SplashScreenInitialtate) {
+                      cubit.loadNearbyPlaces();
                     }
-                  }
-                  return const SizedBox();
-                }),
-          ],
+                    if (state is SplashCompleteSearch) {
+                      if (mainCubit.navigateToHome) {
+                        _navigateToMain(mainCubit.selectedBottomNavItem);
+                      } else {
+                        _scheduleNavigateLogin();
+                      }
+                    }
+                    return const SizedBox();
+                  }),
+            ],
+          ),
         ),
       ),
     );
